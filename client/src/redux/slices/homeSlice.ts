bbc3ac77337e72729778dcda8c1b7fcf2f0cf42d
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2022-12-10 21:54:43 +0700

    Configure necessary packages for client

---
+++ client/src/redux/slices/homeSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { GET_ITEMS } from '../../api/endpoints';
+import { apiSlice } from './apiSlice';
+
+const initialState = {
+  items: [],
+};
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {},
+});
+
+export const homeApi = apiSlice.injectEndpoints({
+  endpoints: (build) => ({
+    getItems: build.query({
+      query: (id) => ({
+        url: `${GET_ITEMS}/${id}`,
+        method: 'GET',
+      }),
+      transformResponse: (response, meta: any, arg: any) => {
+        return response;
+      },
+    }),
+  }),
+});
+
+export default homeSlice.reducer;
+
+export const { useGetItemsQuery } = homeApi;
---
+++ client/src/routes/index.ts
@@ -0,0 +1,10 @@
+import Home from '../containers/UserTemplate/Home';
+
+const routesUser = [
+  {
+    path: '/',
+    component: Home,
+  },
+];
+
+export { routesUser };
