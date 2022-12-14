cc68773abcdf0d80be5a5a1d601869b92fd2aa68
phineasla <63248715+phineasla@users.noreply.github.com>
2022-12-14 21:21:37 +0700

    Migrate services to another repository

--- services/src/common/pagination/pagination.dto.ts
+++
@@ -1,7 +0,0 @@
-import { DEFAULT_PAGINATION_PAGE, DEFAULT_PAGINATION_SIZE } from '../constants';
-
-export class PagingationDto {
-  page: number = DEFAULT_PAGINATION_PAGE;
-
-  size: number = DEFAULT_PAGINATION_SIZE;
-}
