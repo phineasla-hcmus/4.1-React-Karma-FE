bd65272afb73bf52e1ed59992c1c8adcaefc51ce
Ho Lam Bao Khuyen <63384365+HoLamBaoKhuyen@users.noreply.github.com>
2022-12-15 11:03:14 +0700

    remove hash sign from routes

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,5 +1,6 @@
 // in src/App.tsx
 import React from "react";
+import { createBrowserHistory } from "history";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { BankerList } from "./components/banker/BankerList";
@@ -9,8 +10,14 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { authProvider } from "./auth/authProvider";
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
+const history = createBrowserHistory();
+
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+  <Admin
+    history={history}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+  >
     <Resource
       name="users"
       list={BankerList}
