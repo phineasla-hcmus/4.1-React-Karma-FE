2ed717dfecd3df2ab6aa9f0ec06691bc2cf68927
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2022-12-27 13:46:19 +0700

    Add UI for receiver management

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import ChangePassword from './pages/ChangePassword';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
+import ReceiverManagement from './pages/Home/ReceiverManagement';
 import TransferHistory from './pages/Home/TransferHistory';
 import TransferMoney from './pages/Home/TransferMoney';
 import Login from './pages/Login';
@@ -20,6 +21,7 @@ function App() {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/change-password" element={<ChangePassword />} />
       <Route path="/transaction-history" element={<TransferHistory />} />
+      <Route path="/receiver-management" element={<ReceiverManagement />} />
       <Route path="/transfer" element={<TransferMoney />} />
     </Routes>
   );
---
+++ apps/client/src/mocks/index.ts
@@ -0,0 +1,18 @@
+export const RECEIVER_LIST = [
+  {
+    name: 'Ho Lam Bao Khuyen',
+    accountNumber: '123456789',
+  },
+  {
+    name: 'La Ngoc Hong Phuc',
+    accountNumber: '123456780',
+  },
+  {
+    name: 'Nguyen Thu Thao Chau',
+    accountNumber: '123456777',
+  },
+  {
+    name: 'Nguyen Mai Xuan Huyen',
+    accountNumber: '123456721',
+  },
+];
