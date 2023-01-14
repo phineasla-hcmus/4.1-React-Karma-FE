9867abd28b960866dd22328ec477896b6187ac14
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2023-01-14 09:50:01 +0700

    Deactivate account

--- apps/client/src/pages/Profile/index.tsx
+++ apps/client/src/pages/Profile/index.tsx
@@ -1,5 +1,17 @@
-import { Box, Button, Card, Typography } from '@mui/material';
-import React, { useMemo } from 'react';
+import {
+  Backdrop,
+  Box,
+  Button,
+  Card,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
+import React, { useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import AsyncDataRenderer from '../../components/AsyncDataRenderer';
@@ -8,13 +20,37 @@ import {
   StyledBreadCrumbs,
   StyledContentWrapper,
 } from '../../components/styles';
-import { useUserInfoQuery } from '../../redux/slices/authSlice';
+import {
+  useDeactivateAccountMutation,
+  useUserInfoQuery,
+} from '../../redux/slices/authSlice';
 
 export default function Profile() {
   const { isLoading, data: { data: userInfoData = {} } = {} } =
     useUserInfoQuery({});
 
+  const [openDeactivateDialog, setOpenDeactivateDialog] = useState(false);
+
   const userInfo = useMemo(() => userInfoData, [userInfoData]);
+
+  const [deActivateAccount, { isLoading: deactivateLoading }] =
+    useDeactivateAccountMutation();
+
+  const handleClickOpenDeactivateDialog = () => {
+    setOpenDeactivateDialog(true);
+  };
+
+  const handleCloseDeactvateDialog = () => {
+    setOpenDeactivateDialog(false);
+  };
+
+  const handleDeactivateAccount = async () => {
+    try {
+      await deActivateAccount({});
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
 
   return (
     <Layout>
@@ -48,6 +84,7 @@ export default function Profile() {
                 variant="contained"
                 color="error"
                 sx={{ marginTop: '1rem' }}
+                onClick={handleClickOpenDeactivateDialog}
               >
                 Đóng tài khoản
               </Button>
@@ -55,6 +92,28 @@ export default function Profile() {
           </Card>
         </AsyncDataRenderer>
       </StyledContentWrapper>
+      <Dialog open={openDeactivateDialog} onClose={handleCloseDeactvateDialog}>
+        <DialogTitle>Đóng tài khoản</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Bạn có chắc muốn đóng tài khoản?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ marginRight: '1rem' }}>
+          <Button variant="outlined" onClick={handleCloseDeactvateDialog}>
+            Hủy
+          </Button>
+          <Button variant="contained" onClick={handleDeactivateAccount}>
+            Xác nhận
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={deactivateLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </Layout>
   );
 }
